161e0d4cc71e44feb1c04a4761b9da3e6e314648
ShaMAD <at.shamad@gmail.com>
2022-12-13 23:38:39 +0300

    init 47

---
+++ Lesson7.HomeWork47/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 47.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//m = 3, n = 4.
+
+//0,5 7 -2 -0,2
+
+//1 -3,3 8 -9,9
+
+//8 7,8 -7,1 9
+
+namespace Lesson7.HomeWork47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

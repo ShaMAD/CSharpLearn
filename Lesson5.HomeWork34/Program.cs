c62c24ca50c89e8349d104e6c62606364c0e1b92
ShaMAD <at.shamad@gmail.com>
2022-12-13 16:25:08 +0300

    Добавьте файлы проекта.

---
+++ Lesson5.HomeWork34/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.Напишите программу, которая покажет количество чётных чисел в массиве.
+//  [345, 897, 568, 234] -> 2
+
+
+namespace Lesson5.HomeWork34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intRandomArray = new int[10];
+
+            for (int i = 0; i < intRandomArray.Length; i++)
+            {
+                Random random = new Random();
+                intRandomArray[i] = random.Next(100, 1000);
+            }
+
+            var oddNumbersCount = intRandomArray
+                .Where(e => e % 2 == 0)
+                .Count();
+
+            foreach (var item in intRandomArray)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.Write("-> ");
+            Console.WriteLine(oddNumbersCount);
+        }
+    }
+}

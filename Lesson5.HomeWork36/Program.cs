d4da9292e78d66f453dbb231e236287c59660126
ShaMAD <at.shamad@gmail.com>
2022-12-13 18:41:02 +0300

    Задача 38 + создание метода InitiateAndFillArray()

--- Lesson5.HomeWork36/Program.cs
+++ Lesson5.HomeWork36/Program.cs
@@ -2,7 +2,6 @@
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6]-> 0
 
-
 using System;
 
 namespace Lesson5.HomeWork36
@@ -10,25 +9,42 @@ namespace Lesson5.HomeWork36
     internal class Program
     {
         static void Main(string[] args)
+        { 
+            int[] intRandomArray = InitiateAndFillArray(5, -99, 100);
+
+            int oddPositionSumm = OddPositionSumm(intRandomArray);
+
+            Console.WriteLine($"Сумма нечётных позиций {intRandomArray.Length} элементов массива:");
+            Console.Write("[");
+
+            foreach (var item in intRandomArray)
+                Console.Write(" " + item);
+
+            Console.WriteLine(" ] -> " + oddPositionSumm);
+
+
+            int OddPositionSumm(int[] array)
+            {
+                int oddPositionSumm = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                    oddPositionSumm += (i % 2 == 1) ? array[i] : 0;
+                return oddPositionSumm;
+            }
+        }
+        /// <summary>
+        /// Задание размера массива и заполение случайными значениями
+        /// </summary>
+        /// <param name="length">Размер массива</param>
+        /// <param name="minvalue">Минимальное число</param>
+        /// <param name="maxvalue">Максимальное число</param>
+        /// <returns>Заполненный массив</returns>
+        private static int[] InitiateAndFillArray(int length, int minvalue, int maxvalue)
         {
-            int[] intRandomArray = new int[4];
-
-            for (int i = 0; i < intRandomArray.Length; i++)
-            {
-                Random random = new Random();
-                intRandomArray[i] = random.Next(-99, 100);
-            }
-
-            int oddPositionSumm = 0;
-
-            for (int i = 0; i < intRandomArray.Length; i++)
-            {
-                Console.Write(intRandomArray[i] + " ");
-                oddPositionSumm += (i % 2 == 1) ? intRandomArray[i] : 0;
-            }
-
-            Console.Write("-> ");
-            Console.WriteLine(oddPositionSumm);
+            int[] array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = new Random().Next(minvalue, maxvalue);
+            return array;
         }
     }
 }
---
+++ Lesson5.HomeWork38/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+using System;
+
+namespace Lesson5.HomeWork38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intRandomArray = InitiateAndFillArray(4, 1, 100);
+
+            int raznostMinMax = RaznostMinMax(intRandomArray);
+
+            Console.WriteLine($"Разница между максимальным и минимальным элементом массива:");
+            Console.Write("[");            
+
+            foreach (var item in intRandomArray)
+                Console.Write(" "+ item);
+
+            Console.WriteLine(" ] -> " + raznostMinMax);
+
+
+            int RaznostMinMax(int[] array)
+            {
+                int minvalue = array[0], maxvalue = array[0];
+
+                for (int i = 1; i < array.Length; i++)
+                {
+                    maxvalue = (maxvalue > array[i]) ? maxvalue : array[i];
+                    minvalue = (minvalue < array[i]) ? minvalue : array[i];
+                }
+                return maxvalue - minvalue;
+            }
+        }
+        /// <summary>
+        /// Задание размера массива и заполение случайными значениями
+        /// </summary>
+        /// <param name="length">Размер массива</param>
+        /// <param name="minvalue">Минимальное число</param>
+        /// <param name="maxvalue">Максимальное число</param>
+        /// <returns>Заполненный массив</returns>
+        private static int[] InitiateAndFillArray(int length, int minvalue, int maxvalue)
+        {
+            int[] array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = new Random().Next(minvalue, maxvalue);
+            return array;
+        }
+    }
+}

e3440b3f7bfef06c20cf9e396d00538ac82a0c03
ShaMAD <at.shamad@gmail.com>
2022-12-14 13:55:47 +0300

    Задача 47

--- Lesson7.HomeWork47/Program.cs
+++ Lesson7.HomeWork47/Program.cs
@@ -8,13 +8,96 @@
 
 //8 7,8 -7,1 9
 
+using System;
+
 namespace Lesson7.HomeWork47
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double[,] doubleArray = MultiArray.InitiateAndFillMultiArrayDouble(
+                length: 5,
+                heigth: 4,
+                minvalue: 0,
+                maxvalue: 10);
+
+            MultiArray.PrintMultiArrayDouble(doubleArray);
+        }
+
+
+    }
+
+    class MultiArray
+    {
+        /// <summary>
+        /// Печать двумерного массива
+        /// </summary>
+        /// <param name="array">Консольный вывод</param>
+        public static void PrintMultiArrayDouble(double[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="heigth"></param>
+        /// <param name="minvalue"></param>
+        /// <param name="maxvalue"></param>
+        /// <returns></returns>
+        public static double[,] InitiateAndFillMultiArrayDouble(int length, int heigth, int minvalue, int maxvalue)
+        {
+            double[,] array = new double[length, heigth];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = Math.Round(new Random().NextDouble() + new Random().Next(minvalue, maxvalue), 2);
+                }
+
+
+            return array;
         }
     }
+    class SingleArray
+    {
+        /// <summary>
+        /// Задание размера массива и заполение случайными значениями
+        /// </summary>
+        /// <param name="length">Размер массива</param>
+        /// <param name="minvalue">Минимальное число</param>
+        /// <param name="maxvalue">Максимальное число</param>
+        /// <returns>Заполненный массив</returns>
+        public static int[] InitiateAndFillArrayInteger(int length, int minvalue, int maxvalue)
+        {
+            int[] array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = new Random().Next(minvalue, maxvalue);
+            return array;
+        }
+
+        /// <summary>
+        /// Печать одномерного int массива
+        /// </summary>
+        /// <param name="array"></param>
+        public static void PrintArrayInteger(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+    }
+
 }
+        

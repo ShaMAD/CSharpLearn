afdea109141b1ea83fec3e2df2c6fa246708ad89
Антон Бармашов <anton@barmashov.su>
2022-12-16 16:53:53 +0300

    Задача 66

---
+++ Lesson9.HomeWork66/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15-> 120
+//M = 4; N = 8. -> 30
+
+
+namespace Lesson9.HomeWork66
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = UserInputInteger("Введите число M: ");
+            int n = UserInputInteger("Введите число N: ");
+
+            Console.Write($"Сумма натуральных элементов между '{m}' и '{n}' равна "+SummElem(m, n));
+
+            int SummElem(int m, int n)
+            {
+                if (m >= n)
+                    return m;
+                return m + SummElem(m+1,n);
+            }
+        }
+
+
+        /// <summary>
+        /// Ввод числа с клавиатуры
+        /// </summary>
+        /// <returns>Число integer</returns>
+        public static int UserInputInteger(string text)
+        {
+            int inputInteger = 0;
+            try
+            {
+                Console.Write(text);
+                string userInput = Console.ReadLine()!;
+                inputInteger = int.Parse(userInput);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка преобразования строки в число!");
+                Environment.Exit(-100);
+            }
+            return inputInteger;
+        }
+
+    }
+}
